58baa50a1f16b057882efa297ada5a6834fd9241
P42 <lars.grammel@gmail.com>
2021-03-18 13:02:39 +0100

    Use Template Literals
    **Template literals are string templates that can include variables and expressions.** They can replace string concatenation and are often easier to read. Example: `` `Hello, ${name}!` ``
    
    This refactoring supports converting the first line of concatenated strings into template literals or shortened strings:
    * concatenated string literals will be combined, e.g. `"a" + "b"` will become `"ab"`
    * concatenated expressions and variables will be embedded in templates, e.g. `"a" + x + "b"` will become `` `a${x}b` ``
    * unnecesary escaping of quotes is removed when string are combined or transformed into template literals
    
    Learn More: [Template Literals (MDN)](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)

--- src/modules/gmail/index.js
+++ src/modules/gmail/index.js
@@ -138,7 +138,7 @@ function b64DecodeUnicode(str) {
   return decodeURIComponent(
     atob(str.replace(/_/g, "/").replace(/-/g, "+"))
       .split("")
-      .map(c => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+      .map(c => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`)
       .join("")
   );
 }
